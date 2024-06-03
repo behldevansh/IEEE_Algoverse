f1e60d7d2f9cd705dadb07a72e7172d4e2b2bc7e
ANSHIKA GUPTA <anshikag.2118@gmail.com>
2024-06-03 11:32:27 +0530

    updated

--- src/components/database.js
+++ src/components/database.js
@@ -1,38 +1,51 @@
 export const Leaderboard = [
-    {
-        name: "Shubham Tandon",
-        branch: "CSDS",
-        score : 225,
-        img: "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
-        dt: "2024-02-31"
-    },
-    {
-        name: "Gaurav Kapoor",
-        branch: "CSDS",
-        score : 190,
-        img: "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
-        dt: "2024-01-01"
-    },
-    {
-        name: "Sumukh Bansal",
-        branch: "MAC",
-        score : 110,
-        img: "https://images.unsplash.com/photo-1534528741775-53994a69daeb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
-        dt: "2021-08-17"
-    },
-    {
-        name: "Aryan Negi",
-        branch: "CSE",
-        score : 90,
-        img: "https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
-        dt: "2021-10-23"
-    },
-    {
-        name: "Atul Dhandha",
-        branch: "CSAI",
-        score : 80,
-        img: "https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
-        dt: "2022-01-22"
-    }
-
-]
+  {
+    name: "Bhuwan Chandra Pandey",
+    branch: "ECE",
+    score: 80,
+    img: "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
+    dt: "2024-06-02",
+  },
+  {
+    name: "Adesh",
+    branch: "ME",
+    score: 80,
+    img: "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
+    dt: "2024-06-02",
+  },
+  {
+    name: "Eugene Samuel",
+    branch: "ECE",
+    score: 80,
+    img: "https://images.unsplash.com/photo-1534528741775-53994a69daeb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
+    dt: "2021-06-02",
+  },
+  {
+    name: "Gaurav Kapoor",
+    branch: "CSDS",
+    score: 70,
+    img: "https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
+    dt: "2021-10-23",
+  },
+  {
+    name: "Aman Pandit",
+    branch: "ICE",
+    score: 60,
+    img: "https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
+    dt: "2022-01-22",
+  },
+  {
+    name: "Sumukh Bansal",
+    branch: "MAC",
+    score: 60,
+    img: "https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
+    dt: "2022-01-22",
+  },
+  {
+    name: "vaibhav Sharma",
+    branch: "CSE",
+    score: 60,
+    img: "https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
+    dt: "2022-01-22",
+  },
+];
