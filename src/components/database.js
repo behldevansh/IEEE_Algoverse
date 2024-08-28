eae9ed347319b5067229582e44a4fabda8e091fa
ANSHIKA GUPTA <anshikag.2118@gmail.com>
2024-08-28 10:48:47 +0530

    Leaderboard updated

--- src/components/database.js
+++ src/components/database.js
@@ -2,28 +2,21 @@ export const Leaderboard = [
   {
     name: "Aman Pandit",
     branch: "ICE",
-    score: 220,
+    score: 240,
     img: "https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
     dt: "2022-06-02",
   },
-  {
-    name: "Eugene Samuel",
-    branch: "ECE",
-    score: 220,
-    img: "https://images.unsplash.com/photo-1534528741775-53994a69daeb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
-    dt: "2021-06-02",
-  },
-  {
-    name: "Bhuwan Chandra Pandey",
-    branch: "ECE",
-    score: 220,
-    img: "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
-    dt: "2024-06-02",
-  },
+  // {
+  //   name: "Eugene Samuel",
+  //   branch: "ECE",
+  //   score: 220,
+  //   img: "https://images.unsplash.com/photo-1534528741775-53994a69daeb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
+  //   dt: "2021-06-02",
+  // },
   {
     name: "Shubham Tandon",
     branch: "CSDS",
-    score: 200,
+    score: 230,
     img: "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
     dt: "2024-06-02",
   },
@@ -33,6 +26,13 @@ export const Leaderboard = [
     score: 190,
     img: "https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
     dt: "2021-06-02",
+  },
+  {
+    name: "Bhuwan Chandra Pandey",
+    branch: "ECE",
+    score: 200,
+    img: "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
+    dt: "2024-06-02",
   },
 
   // {
